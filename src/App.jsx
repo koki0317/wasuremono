e33942efcbe8ea72eabe4dc90f49cc2f4eddf5ed
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-10-07 22:23:51 +0900

    created lists page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import List from "./pages/List";
 import Footer from "./components/Footer";
 import TopButton from "./components/TopButton";
+import Lists from "./pages/Lists";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Top />} />
         <Route path="/list" element={<List />} />
+        <Route path="/lists" element={<Lists />} />
         <Route path="/practice" element={<TopButton />} />
       </Routes>
       <Footer />
---
+++ src/pages/Lists.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.h1`
+  text-align: center;
+  margin-top: 10px;
+`;
+const ListWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const List = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #6c63ff;
+  width: 80%;
+  padding: 5px 10px;
+`;
+const ListTitle = styled.h2``;
+const Date = styled.span``;
+const Image = styled.img`
+  height: 200px;
+`;
+
+const TitleImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const Lists = () => {
+  return (
+    <Container>
+      <TitleImageWrapper>
+        <Title>Your lists</Title>
+        <Image src="undraw_Note_list_re_r4u9.png" />
+      </TitleImageWrapper>
+      <ListWrapper>
+        <List>
+          <ListTitle>Travel abroad</ListTitle>
+          <Date>today</Date>
+        </List>
+      </ListWrapper>
+    </Container>
+  );
+};
+
+export default Lists;
