de74ea14fdbc217d9ff97460c8eeb5dc9b60942d
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-10-07 22:39:11 +0900

    Creating a function that enable us to create a new list

---
+++ src/components/CreateList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.h1``;
+
+const CreateList = () => {
+  return (
+    <Container>
+      <Title>Create a new list.</Title>
+    </Container>
+  );
+};
+
+export default CreateList;
