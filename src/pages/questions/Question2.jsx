0cdd0acdee9fab533ded1cef25ca5ed077eba11f
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-10-27 08:09:54 +0900

    Used the button component for question 2 instead of creating it

--- src/pages/questions/Question2.jsx
+++ src/pages/questions/Question2.jsx
@@ -23,31 +23,6 @@ const Title = styled.h1`
   margin-bottom: 30px;
 `;
 
-const Button = styled.button`
-  width: 200px;
-  background-color: #6c63ff;
-  border: none;
-  border-radius: 3px;
-  padding: 5px 0;
-  font-size: 20px;
-  color: white;
-  font-weight: 600;
-  box-shadow: 0 5px 0 #c2c0c0;
-  cursor: pointer;
-  transition: all 0.5s ease;
-  opacity: 0.93;
-  margin-bottom: 30px;
-
-  &:active {
-    transform: translateY(4px);
-    border-bottom: none;
-  }
-
-  &:hover {
-    opacity: 1;
-  }
-`;
-
 const Question2 = ({ addAnswer, answer }) => {
   const [input, setInput] = useState("");
 
@@ -88,12 +63,13 @@ const Question2 = ({ addAnswer, answer }) => {
           </Select>
         </FormControl>
         <Link to="/create/question3">
-          <Button
+          <ComponentButton
             onClick={() => handleClick()}
             disabled={!input ? true : false}
+            content={"NEXT"}
           >
             NEXT
-          </Button>
+          </ComponentButton>
         </Link>
         <Link to="/create/question1">
           <ComponentButton
