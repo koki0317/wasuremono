82932b02c67155040b9aa241bddbc1703e652195
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-10-30 22:05:18 +0900

    Fixed the bug

--- src/pages/CreateList.jsx
+++ src/pages/CreateList.jsx
@@ -41,13 +41,13 @@ const containerVariants = {
   visible: {
     opacity: 1,
     transition: {
-      delay: 0.5,
-      duration: 1,
+      delay: 1.5,
+      duration: 1.5,
     },
-    exit: {
-      x: "-100vh",
-      transition: { ease: "easeInOut" },
-    },
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
   },
 };
 
