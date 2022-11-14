1338999e44b353d55f396bd2f014e61499a167f4
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-11-14 17:32:59 +0900

    Save the form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import ComponentButton from "./Button";
 
 const Container = styled.div``;
 const Form = styled.form``;
 const Label = styled.label``;
 const Input = styled.input``;
+const Title = styled.h1`
+  text-align: center;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const LoginForm = () => {
   return (
     <Container>
       <Form>
-        <Label></Label>
-        <Input></Input>
+        <Wrapper>
+          <Title>Login</Title>
+          <Label>E-Mail</Label>
+          <Input />
+          <Label>Password</Label>
+          <Input style={{ marginBottom: "30px" }} />
+          <ComponentButton content={"Login"} />
+        </Wrapper>
       </Form>
     </Container>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import LoginForm from "../components/LoginForm";
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 const Login = () => {
-  return <LoginForm />;
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  );
 };
 
 export default Login;
